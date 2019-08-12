03e6fdf42bc5ce7604667df06cfbaf91cd87f994
Marco <realmarko@gmail.com>
2019-08-11 20:17:33 -0400

    Exercise1

---
+++ Exercises/Exercises/Exercise1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Exercises
+{
+    public class Exercise1
+    {
+        public void Sum()
+        {
+            int result = 0;
+            StringBuilder builder = new StringBuilder();
+
+            for (int k = 1; k <= 10; k++)
+            {
+                int number = 0;
+                number = Convert.ToInt32(Math.Pow(-1, k + 1));
+                builder.Append(Convert.ToInt32(Math.Pow(-1, k + 1)));
+                builder.Append("/" + (2 * k - 1));
+                if (number < 0 && k < 10)
+                    builder.Append("+");
+                result += Convert.ToInt32(Math.Pow(-1, k + 1)) / 2 * k - 1;
+            }
+            result = result * 4;
+            Console.Write(builder);
+            Console.ReadLine();
+        }
+    }
+}
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercises.Exercises;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
+            //Addition
+            Exercise1 exercise1 = new Exercise1();
+            exercise1.Sum();
         }
     }
 }
