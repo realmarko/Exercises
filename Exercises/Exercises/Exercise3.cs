543f2b3d2808a8b8f3e1c2a2b257b4f43511075b
Marco <realmarko@gmail.com>
2019-08-11 20:53:41 -0400

    Exercise3

---
+++ Exercises/Exercises/Exercise3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Exercises
+{
+    public class Exercise3
+    {
+        /// <summary>
+        /// Fin the common elements in the arrays
+        /// </summary>
+        /// <param name="a">First array</param>
+        /// <param name="b">Second array</param>
+        /// <param name="c">Third array</param>
+        /// <param name="d">Fourth array</param>
+        /// <returns></returns>
+        public int[] FindCommonElements(int[] a, int[] b, int[] c, int[] d)
+        {
+            IDictionary<int, int> dictionary = new Dictionary<int, int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                dictionary[a[i]] = 1;
+            }
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (dictionary.ContainsKey(b[i]) && dictionary[b[i]] == 1)
+                {
+                    dictionary[b[i]] = 2;
+                }
+            }
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (dictionary.ContainsKey(c[i]) && dictionary[c[i]] == 2)
+                {
+                    dictionary[c[i]] = 3;
+                }
+
+            }
+            for (int i = 0; i < d.Length; i++)
+            {
+                if (dictionary.ContainsKey(d[i]) && dictionary[d[i]] == 3)
+                {
+                    dictionary[d[i]] = 4;
+
+                }
+
+            }
+            foreach (var item in dictionary.Where(x => x.Value == 4).Select(y => y.Key).ToArray())
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+            return dictionary.Where(x => x.Value == 4).Select(y => y.Key).ToArray();
+        }
+    }
+}
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -18,12 +18,23 @@ namespace Exercises
 
             //Exercise2 
             Exercise2 exercise2 = new Exercise2();
-            //string original = "uno";
-            string original = "aabcccccaaa";
-            string compressed = exercise2.Compress(original);
-            string finalResult = exercise2.EvaluateSize(original, compressed);
-            Console.Write(finalResult);
-            Console.ReadLine();
+            ////string original = "uno";
+            //string original = "aabcccccaaa";
+            //string compressed = exercise2.Compress(original);
+            //string finalResult = exercise2.EvaluateSize(original, compressed);
+            //Console.Write(finalResult);
+            //Console.ReadLine();
+
+
+            ////Excercise3
+            Exercise3 exercise3 = new Exercise3();
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            int[] b = { 3, 2, 9, 3, 7, 11, 15, 13, 23, 27, 32, 35, 36, 37 };
+            int[] c = { 1, 3, 5, 7, 11, 13, 15, 17, 19, 21, 23, 27, 35, 37 };
+            int[] d = { 9, 17, 32, 7, 2, 3, 1, 45, 46, 47, 55, 60, 6, 17 };
+
+            int[] commonNumbers = exercise3.FindCommonElements(a, b, c, d);
+
         }
     }
 }
